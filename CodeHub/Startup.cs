68b8fbaa631b9336024d0f17de03c3ede14315ad
Hakan Ak <hakanak54@gmail.com>
2020-05-03 12:45:25 +0300

    Add project files.

---
+++ CodeHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeHub.Startup))]
+namespace CodeHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
